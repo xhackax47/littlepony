73eeb487818d6e54c4dee7da3aa5b79049b2a0d0
CHAABI Samy <samy_chaabi@yahoo.fr>
2018-11-11 01:47:48 +0100

    Login/register spring ok, angular login/register reponse HTTP 401

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -13,12 +13,12 @@ export class UserService {
   authenticated = false;
 
   httpOptions = {
-    headers: new HttpHeaders().set('Content-type','application/json')
+    headers: new HttpHeaders().set('Content-type', 'application/json')
   }
 
-  constructor(private http: HttpClient, private router: Router) {}
-  
-  getAllUsers(): Observable<Array<User>>{
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getAllUsers(): Observable<Array<User>> {
     return this.http.get<Array<User>>(this.url + '/', this.httpOptions);
     // return of(this.tabRaces);
   }
@@ -31,32 +31,38 @@ export class UserService {
     return this.http.get<User>(this.url + '/user/' + name, this.httpOptions);
   }
 
-  addUser(user: User): void{
-    this.http.post(this.url + '/', user, this.httpOptions).subscribe(() => this.router.navigate(['/']));
+  addUser(user: User): void {
+    this.http.post(this.url + '/register', user, this.httpOptions).subscribe(() => this.router.navigate(['/']));
   }
 
-  updateUser(id: number ,user: User): void{
+  updateUser(id: number, user: User): void {
     this.http.put(this.url + '/' + id, user, this.httpOptions).subscribe(() => this.router.navigate(['/']));
   }
 
-  deleteUser(id: number): void{
+  deleteUser(id: number): void {
     this.http.delete(this.url + '/' + id, this.httpOptions).subscribe(() => this.router.navigate(['/']));
   }
 
   authenticate(credentials, callback) {
 
     const headers = new HttpHeaders(credentials ? {
-        authorization : 'Basic ' + btoa(credentials.username + ':' + credentials.password)
+      authorization: 'Basic ' + btoa(credentials.username + ':' + credentials.password)
     } : {});
 
-    this.http.get('user', {headers: headers}).subscribe(response => {
-        if (response['name']) {
-            this.authenticated = true;
-        } else {
-            this.authenticated = false;
-        }
-        return callback && callback();
+    this.http.get('user', { headers: headers }).subscribe(response => {
+      if (response['name']) {
+        this.authenticated = true;
+      } else {
+        this.authenticated = false;
+      }
+      return callback && callback();
     });
 
-}
+  }
+
+  login(usernameOrEmail: string, password: string): Observable<Object> {
+    const credentials = {usernameOrEmail: usernameOrEmail, password: password};
+    console.log("Tentative de login de :" + usernameOrEmail);
+    return this.http.post(this.url + '/login' , this.httpOptions);
+  }
 }
